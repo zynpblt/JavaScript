47957182d2bb490cc750f1e6d45572648395e773
zynpblt <fzaydin6636@gmail.com>
2021-09-25 21:22:31 +0100

    Lesson4V2

--- ders4_dom/ornek2_domManipule/app.js
+++ ders4_dom/ornek2_domManipule/app.js
@@ -16,3 +16,17 @@ document.querySelector(".off").onclick=function(){
 document.querySelector(".speak").onclick = function () {
     resim.src = " ./img/telfirlat.gif";
 }
+//ekle butonuna basildiginda yeni bir li eklentisi
+const liste=document.querySelector(".liste");
+document.querySelector(".ekle").
+onclick=function(){
+   
+    const satir=document.querySelector(".dil");
+
+    liste.innerHTML=liste.innerHTML+`<li>${satir.value}</li>`;
+    satir.value="";
+}
+//sil butonuna tiklandiginda li elemani silinsin
+document.querySelector(".sil").onclick=function(){
+liste.removeChild(liste.lastChild);
+}

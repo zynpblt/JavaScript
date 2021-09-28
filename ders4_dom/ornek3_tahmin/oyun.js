29214e5c0c68c8263b889cc88a57143d6c1ba2d7
zynpblt <fzaydin6636@gmail.com>
2021-09-28 20:19:08 +0100

    example3/Guessv1

--- ders4_dom/ornek3_tahmin/oyun.js
+++ ders4_dom/ornek3_tahmin/oyun.js
@@ -1,32 +1,38 @@
 
-//bilgisayar 1-20 arasında bir sayı tut
+// bilgisayar 1-20 arasinda bir sayi tut
 
-let rastgeleSayi=   Math.floor(Math.random()*20+1);
-let puanPc=10;
- let rekorPc=0;
+let rastgeleSayi = Math.floor(Math.random() * 20 + 1);
+let puanPc = 10;
+let rekorPc = 0;
 
- //her kontrol et butonuna tıklandığında çalıştırdım
- document.querySelector(".kontrol").onclick=function()
- {
-   //tahminim doğruysa
-   const tahmin=document.querySelector(".tahmin").value;
-   if(tahmin==rastgeleSayi){
-document.querySelector("body").style.backgroundColor="green";
-document.querySelector(".question").textContent=rastgeleSayi;
-document.querySelector(".mesaj").textContent="🎉Tebrikler Bildiniz";
+// her kontrol et butonuna tiklandiginda calisacak fonksiyon
+document.querySelector(".kontrol").onclick = function () {
+  // tahminim dogruysa
+  const tahmin = document.querySelector(".tahmin").value;
+  if (tahmin == rastgeleSayi) {
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".question").textContent = rastgeleSayi;
+    document.querySelector(".mesaj").textContent = "🎊Tebrikler Bildiniz";
 
-//rekoru güncelle
-if(puanPc>rekorPc){
-  rekorPc=puanPc;
-  document.querySelector(".rekor-skor").textContent=puanPc;
-}
+    //rekoru guncelle
+    if (puanPc > rekorPc) {
+      rekorPc = puanPc;
+      document.querySelector(".rekor-skor").textContent = PuanPc;
+    }
+  }
+  // tahmin yalissa
+  else {
+    if (puanPc > 1) {
+      puanPc--;
+      let mesaj = document.querySelector(".mesaj");
 
-   }
-//tahmin yanlış ise
-else{
-  if(puanPc>1){
-  puanPc--;
-let mesaj= document.querySelector(".mesaj");
-tahmin < rastgeleSayi ? (mesaj.textContent = "Arttır 🆙") : (mesaj.textContent = "Azalt ⬇️");
-document.querySelector(".skor").textContent=puanPc;
-
+      tahmin < rastgeleSayi
+        ? (mesaj.textContent = "Artir 📈")
+        : (mesaj.textContent = "Azalt 📉");
+      document.querySelector(".skor").textContent = puanPc;
+    } else {
+      mesaj.textContent = "Oyunu kaybettiniz😢";
+      document.querySelector(".skor").textContent =0;
+    }
+  }
+};

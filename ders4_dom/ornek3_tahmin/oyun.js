21d2146a239f2f9c1358f79c64cf52d3f1ee6580
zynpblt <fzaydin6636@gmail.com>
2021-09-28 20:17:42 +0100

    example3/Guess

---
+++ ders4_dom/ornek3_tahmin/oyun.js
@@ -0,0 +1,32 @@
+
+//bilgisayar 1-20 arasında bir sayı tut
+
+let rastgeleSayi=   Math.floor(Math.random()*20+1);
+let puanPc=10;
+ let rekorPc=0;
+
+ //her kontrol et butonuna tıklandığında çalıştırdım
+ document.querySelector(".kontrol").onclick=function()
+ {
+   //tahminim doğruysa
+   const tahmin=document.querySelector(".tahmin").value;
+   if(tahmin==rastgeleSayi){
+document.querySelector("body").style.backgroundColor="green";
+document.querySelector(".question").textContent=rastgeleSayi;
+document.querySelector(".mesaj").textContent="🎉Tebrikler Bildiniz";
+
+//rekoru güncelle
+if(puanPc>rekorPc){
+  rekorPc=puanPc;
+  document.querySelector(".rekor-skor").textContent=puanPc;
+}
+
+   }
+//tahmin yanlış ise
+else{
+  if(puanPc>1){
+  puanPc--;
+let mesaj= document.querySelector(".mesaj");
+tahmin < rastgeleSayi ? (mesaj.textContent = "Arttır 🆙") : (mesaj.textContent = "Azalt ⬇️");
+document.querySelector(".skor").textContent=puanPc;
+

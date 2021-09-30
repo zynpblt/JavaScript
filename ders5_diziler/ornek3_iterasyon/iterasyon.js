f1869c4f505fd06751a7634d09c1c5165aadf033
zynpblt <fzaydin6636@gmail.com>
2021-09-30 20:16:59 +0100

    example3iterators

--- ders5_diziler/ornek3_iterasyon/iterasyon.js
+++ ders5_diziler/ornek3_iterasyon/iterasyon.js
@@ -1,12 +1,109 @@
 //***********DIZILERDE ITERASYON********** */
 //***************FOR DONGUSU************* */
 
+//const koordinatlar = [120, 100, -100, 220, 330, -50];
+
+//let pozitif=0;
+//let negatif=0;
+//for(let i=0; i<koordinatlar.length; i++){
+
+//    koordinatlar[i]< 0 ? negatif=negatif+koordinatlar[i] : pozitif=pozitif+koordinatlar[i];
+//}
+//console.log(`negatifler toplami:${negatif}pozitifler toplami:${pozitif}`);
+
+/*****************FOR IN DONGUSU */
 const koordinatlar = [120, 100, -100, 220, 330, -50];
 
 let pozitif=0;
 let negatif=0;
-for(let i=0; i<koordinatlar.length; i++){
+for(let i in koordinatlar){
 
     koordinatlar[i]< 0 ? negatif=negatif+koordinatlar[i] : pozitif=pozitif+koordinatlar[i];
 }
-console.log(`negatifler toplami:${negatif}pozitifler toplami:${pozitif}`);
+console.log(`negatifler toplami:${negatif} pozitifler toplami:${pozitif}`);
+
+// SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+// bir dizide saklandığı varsayalım.Bu hayvanları türüne
+// göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//  ana programa döndürecek fonksiyonu yaziniz. Eğer o
+// türden bir hayvan yok ise bulunamadığını yazdırsın.
+
+//const hayvanlar=["aslan","fil","kartal","kanarya","aslan",
+//"aslan","timsah"];
+//const ara=prompt("sayisini merak ettigin hayvanin adini yaz");
+//const bulDondur=function(ara){
+
+
+//let hayvanSayisi=0;
+
+//for (let i in hayvanlar){
+//if (hayvanlar[i]==ara) {
+//hayvanSayisi++;
+//}/
+//}
+//hayvanSayisi==0? alert("aradigin hayvandan bulunamadi"):
+//alert(`aradiginiz ${ara} hayvanindan ${hayvanSayisi} tane bulunmaktadir`);
+//}
+//bulDondur(ara);
+
+//************FOR OF DONGUSU**** */
+// for of dongusu, for in dongusunun bir cok veri 
+//yapisini kapsayacak sekilde guncellenmis halidir.
+ //Bu dongude dizi icersindeki veriye erisirken
+ // indisleme kullanmaya gerek yoktur.
+
+ let arabalar=["BMW","Volvo","Mini"];
+ for(let i of arabalar){
+     console.log(i);
+ }
+
+ //***********FOREACH METODU******** */
+ //! FOREACH methodu orjinal diziyi bozmaz
+
+ let ogrenciler=["Onur","Hakan","Furkan"];
+ console.log(ogrenciler.forEach((ogrenci)=>console.log(ogrenci)));
+//undefined
+
+ogrenciler.forEach((deger,index)=>console.log(index+".kisi"+deger+"dir."));
+console.log(ogrenciler);
+
+/*********MAP METODU********** */
+
+//!MAP metodu orjinal diziyi degistirmez
+
+//ornek:butun elemanlarin 3 katini aliniz
+
+const rakamlar=[3,5,3,2,6,7,9];
+console.log(rakamlar.map((x)=>x*3));
+
+console.log(rakamlar.map((x,index)=> `${index+1}.elemanin 3 kati ${x*3}`));
+
+//Ornek
+//isimler dizisinin icerisindeki her ismi buyuk harf olarak
+//yeni bir diziye sakla
+
+const isimler = [
+    "Ahmet",
+    "mehmet",
+    "ismet",
+    "SAFFET",
+    "Can",
+    "Canan",
+    "Cavidan"
+  ];
+
+//map metodu bir dizi dondurur
+
+const buyuk=isimler.map((isim)=>isim.toUpperCase());
+console.log(buyuk);
+
+document.querySelector(".baslik").textContent=buyuk.join("->");
+
+/**********FILTER METODU******* */
+
+//Ornek
+
+//fiyatlar dizisindeki 250 den buyuk fiyatlari yazdirAd
+
+const fiyatlar=[120,260,300,322.5,400,768];
+console.log(fiyatlar.filter((t)=>t>250));
